098bd848eca845f68ab413954c022782a180457e
Newboe1 <pantzaid@gmail.com>
2025-11-16 08:32:30 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+https://dashboard.heroku.com/new?team=rahman-xm&template=https%3A%2F%2Fgithub.com%2Fbreshyb%2FSchool
---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'NewBoe',
+  author: '‎',
+  botName: "*NewBoe*",
+  botOwner: 'Marisel', // Your name
+  ownerNumber: '254740007567', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.6.0",
+  updateZipUrl: "https://github.com/breshyb/School/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
